d865b7ad3935f0c8a52360d7b1bbdbf70b01aefa
Callumari58 <thoainguyendt2014@gmail.com>
2025-08-16 07:39:46 +0700

    Scriptdemo12.lua

--- Scriptdemo12.lua
+++ Scriptdemo12.lua
@@ -1,6 +1,4 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
-
-       local Window = MakeWindow({
+local Window = MakeWindow({
          Hub = {
          Title = "TomNeDay",
          Animation = "Youtube: TomNeDay"
